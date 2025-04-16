44519b8da2b6ad69d190b86651c6cc7a30007509
Ivar.0251 <102385567+ivar-0251@users.noreply.github.com>
2025-04-16 08:16:23 +0000

    3commit

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-import sqlite3 as sql
-import os
-
-db = sql.connect("tennisvereniging.sqlite")
-
-cursor = db.cursor()
-
-with open("code.txt", "r") as file:
-    cursor.execute(file.read())
-
-result = cursor.fetchall()
-
-db.close()
-
-i=0
-for item in result:
-    i += 1
-    print(f"{i}. " + str(item) + "\n")
-
-file.close()
+import random
+if input("True or false: ").lower() == random.choice(["true", "false"]): print("Correct")
+else: print("Wrong")
